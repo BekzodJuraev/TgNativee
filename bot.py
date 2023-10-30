bc5a4f33aa44e922c7f9630de170bed9069c936a
Bekzod <105592710+BekzodJuraev@users.noreply.github.com>
2023-10-30 16:45:15 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ from pyrogram import Client
 from pyrogram.enums import ChatAction,ParseMode
 from pyrogram.handlers import MessageHandler
 from pyrogram.types import Message
-api_id = '26340505'
-api_hash = '7960c20df051be9831dbc9919926393b'
+api_id = '#'
+api_hash = '#'
 from pyrogram import filters
 
 client=Client('me_client', api_id, api_hash)
