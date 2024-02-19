d566f3973c5ebc0b5e67893b496ddb446dc31a24
Bekzod <powerzver98@gmail.com>
2024-02-19 09:49:18 +0500

    push for hosting

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -100,7 +100,7 @@ def add_chanel(chanel_link):
                 for key, value in payload.items():
                     files[key] = (None, str(value))
 
-                response = requests.post('https://ebc7-84-54-70-98.ngrok-free.app/api/', files=files)
+                response = requests.post('http://194.163.187.72/api/', files=files)
 
                 with open(file_path, "rb") as photo:
                     client.send_photo("@lsbnvVm9TmhjZDNi", photo)
